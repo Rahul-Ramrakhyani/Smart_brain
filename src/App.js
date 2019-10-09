864ff3be2cbf99b496418842a172f3902246025a
Rahul Ramrakhyani <rahul.ramrakhyani@yahoo.com>
2019-10-09 22:52:39 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -12,13 +12,72 @@ import './App.css';
 const particlesOptions = {
   particles: {
    number: {
-     value: 250,
+     value: 200,
      density: {
       enable: true,
       value_area: 800
    }
+  },
+      "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "opacity_min": 0.1,
+        "sync": true
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed":8,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 1200,
+        "rotateY": 1200
+      }
+    }
   }
- }
 }
 
 const initialState =
