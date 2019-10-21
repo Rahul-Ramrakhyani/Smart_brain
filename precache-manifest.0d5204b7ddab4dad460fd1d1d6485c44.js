700ce1502b5a264d7baac27a346103d2a6da24ca
Rahul Ramrakhyani <rahul.ramrakhyani@yahoo.com>
2019-10-22 03:06:55 +0530

    Updates

---
+++ precache-manifest.0d5204b7ddab4dad460fd1d1d6485c44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f2ba932504b264e4dd70283dabf8a1",
+    "url": "/Smart_brain/index.html"
+  },
+  {
+    "revision": "2559926dc98168c88c2a",
+    "url": "/Smart_brain/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ad6aeb3194cb50c7c342",
+    "url": "/Smart_brain/static/css/main.91232172.chunk.css"
+  },
+  {
+    "revision": "2559926dc98168c88c2a",
+    "url": "/Smart_brain/static/js/2.b6b9a08f.chunk.js"
+  },
+  {
+    "revision": "ad6aeb3194cb50c7c342",
+    "url": "/Smart_brain/static/js/main.926422a4.chunk.js"
+  },
+  {
+    "revision": "c63053247c75938e7be4",
+    "url": "/Smart_brain/static/js/runtime~main.b1175a7c.js"
+  }
+]);
